22e7c047c2617422e3ce7c13473275f10a7dafbc
Jackson Atkins <DMI@Jacksons-MBP.lan>
2020-12-19 11:49:06 +0000

    Display results by rank. Lema and multiword matching.

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module pulley.com/shakesearch
 
 go 1.15
+
+require github.com/agonopol/go-stem v0.0.0-20150630113328-985885018250 // indirect
--- main.go
+++ main.go
@@ -9,7 +9,11 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"regexp"
+	"sort"
 	"strings"
+
+	stemmer "github.com/agonopol/go-stem"
 )
 
 type (
@@ -19,13 +23,14 @@ type (
 	}
 	// ShakespeareLineRec hold individual lines of Shakespear's works
 	ShakespeareLineRec struct {
-		Type         string      `json:"type,omitempty"`
-		LineID       int         `json:"line_id,omitempty"`
-		PlayName     string      `json:"play_name,omitempty"`
-		SpeechNumber interface{} `json:"speech_number,omitempty"`
-		LineNumber   string      `json:"line_number,omitempty"`
-		Speaker      string      `json:"speaker,omitempty"`
-		TextEntry    string      `json:"text_entry,omitempty"`
+		Type         string   `json:"type,omitempty"`
+		LineID       int      `json:"line_id,omitempty"`
+		PlayName     string   `json:"play_name,omitempty"`
+		SpeechNumber string   `json:"speech_number,omitempty"`
+		LineNumber   string   `json:"line_number,omitempty"`
+		Speaker      string   `json:"speaker,omitempty"`
+		TextEntry    string   `json:"text_entry,omitempty"`
+		WordsList    []string `json:"words_list,omitempty"`
 	}
 	// ResultRec hold the data from the database used to fill the template
 	ResultRec struct {
@@ -36,6 +41,7 @@ type (
 		lineNumber string
 		speaker    string
 		lineType   string
+		score      int
 	}
 )
 
@@ -57,7 +63,7 @@ func main() {
 	defer lf.Close()
 
 	// Get input from Database
-	b, err := ioutil.ReadFile("data.json")
+	b, err := ioutil.ReadFile("shakeworks.json")
 	if err != nil {
 		err = errors.New("Failed to open database file")
 		log.Fatal(err)
@@ -111,8 +117,76 @@ func handleSearch(data ShakespeareDataRec) func(w http.ResponseWriter, r *http.R
 	}
 }
 
-// Search method for finding a string within the data
-func (s *ShakespeareDataRec) Search(query string) string {
+// Search main search function
+func (s *ShakespeareDataRec) Search(query string) (results string) {
+
+	var hits []ResultRec
+	queryWords := getWordsSlice(query)
+
+	for i := range s.ShakespeareLine {
+		score := matchScore(s.ShakespeareLine[i].WordsList, queryWords)
+		if score > 0 {
+			hit := ResultRec{
+				lineBefore: s.ShakespeareLine[i-1].TextEntry,
+				hit:        strings.Replace(s.ShakespeareLine[i].TextEntry, query, fmt.Sprintf("<b>%v</b>", query), -1),
+				lineAfter:  s.ShakespeareLine[i+1].TextEntry,
+				playName:   s.ShakespeareLine[i].PlayName,
+				lineNumber: s.ShakespeareLine[i].LineNumber,
+				speaker:    s.ShakespeareLine[i].Speaker,
+				lineType:   s.ShakespeareLine[i].Type,
+				score:      score,
+			}
+			hits = append(hits, hit)
+			// results += hit.FormatHTML(hit)
+		}
+	}
+	sortResults(hits)
+	for _, v := range hits {
+		results += v.FormatHTML(v)
+	}
+	return results
+}
+
+func getWordsSlice(s string) (wordSlice []string) {
+
+	// Make a Regex to say we only want letters, numbers, and spaces
+	reg, err := regexp.Compile("[^a-zA-Z0-9\\s]+")
+	if err != nil {
+		log.Println(err)
+	}
+	processedString := reg.ReplaceAllString(s, "")
+
+	words := strings.Split(processedString, " ")
+
+	for _, v := range words {
+		wordSlice = append(wordSlice, string(stemmer.Stem([]byte(v))))
+	}
+	return wordSlice
+}
+
+func matchScore(data []string, query []string) (score int) {
+	for _, d := range data {
+		for _, q := range query {
+			if d == q {
+				score++
+			}
+		}
+	}
+	return score
+}
+
+func sortResults(hits []ResultRec) {
+	var less func(i, j int) bool
+
+	less = func(i, j int) bool {
+		return hits[i].score > hits[j].score
+	}
+
+	sort.Slice(hits, less)
+}
+
+// SimpleSearch main search function
+func (s *ShakespeareDataRec) SearchSimple(query string) string {
 
 	var results string
 
@@ -145,15 +219,14 @@ func (r *ResultRec) FormatHTML(hit ResultRec) string {
             <div class="result__title">
                 <h2 class="result__heading">%v %v</h2>
                 <div class="result__tag result__tag--1">#%v</div>
-                <div class="result__tag result__tag--2">#%v</div>
-
+				<div class="result__tag result__tag--2">#%v</div>
             </div>
             <p class="result__description">%v</p>
         </div>
         <div class="result__work">
-        	%v
+        	RANK %v
         </div>
-	</figure>`, r.playName, r.lineNumber, r.lineType, r.speaker, text, r.playName)
+	</figure>`, r.playName, r.lineNumber, r.lineType, r.speaker, text, r.score)
 
 	return formatedResult
 
